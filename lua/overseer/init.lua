38d89f4c8e985557da904ef8defd61e1a4586d8d
Steven Arcangeli <stevearc@stevearc.com>
2022-05-24 21:46:45 -0700

    ci: run tests in github action

--- lua/overseer/init.lua
+++ lua/overseer/init.lua
@@ -7,10 +7,10 @@ local window = require("overseer.window")
 local M = {}
 
 -- TODO
--- * Create some better tools for parsing output
 -- * Add a command to pick a completed task and rerun. Include disposed tasks.
 -- * Statusline integration for task status
 -- * Add extension points to the task list actions
+-- * Add actions to set quickfix/loclist from diagnostics or stacktrace
 -- * Many more task templates, especially for tests
 -- * Add a command to quick-action on the last task (and an action to open the terminal). Possibly add a picker to quick-action any task?
 -- * Load VSCode task definitions
