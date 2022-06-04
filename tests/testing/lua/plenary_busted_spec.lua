fccd537feb94ca4d6fecd5eb5abb3b6430053339
Steven Arcangeli <stevearc@stevearc.com>
2022-06-04 09:53:55 -0700

    Fix tests in CI

--- tests/testing/lua/plenary_busted_spec.lua
+++ tests/testing/lua/plenary_busted_spec.lua
@@ -4,11 +4,13 @@ local TEST_STATUS = require("overseer.testing.data").TEST_STATUS
 
 describe("plenary_busted", function()
   it("parses test output", function()
-    local output = [[
+    local filename = debug.getinfo(1, "S").source:sub(2)
+    local output = string.format(
+      [[
 Starting...Scheduling: tests/extensions/nvim_lua_spec.lua
 
 ========================================
-Testing:        /home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua
+Testing:        %s
 Fail    ||      lua can parse test failures
             .../overseer.nvim/tests/testing/lua/plenary_busted_spec.lua:17: Expected objects to be the same.
             Passed in:
@@ -30,32 +32,35 @@ Errors :        0
 ========================================
 
 ========================================
-Testing:        /home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/go/go_test_spec.lua
+Testing:        %s
 Success ||      go_test parses test failures
 
 Success:        1
 Failed :        0
 Errors :        0
 ========================================
-    ]]
+    ]],
+      filename,
+      filename
+    )
     local results = test_utils.run_parser(integration, output)
     assert.are.same({
       tests = {
         {
-          id = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua:lua can parse test failures",
-          filename = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua",
+          id = string.format("%s:lua can parse test failures", filename),
+          filename = filename,
           name = "lua can parse test failures",
           status = TEST_STATUS.FAILURE,
           diagnostics = {
             {
-              filename = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua",
+              filename = filename,
               lnum = 17,
               text = "Expected objects to be the same.\nPassed in:\n(table: 0x7f0d8a8f2c90) { }\nExpected:\n(table: 0x7f0d8a8f3cc8) {\n *[1] = {\n    [filename] = 'my_test.go'\n    [lnum] = 7\n    [text] = 'Expected 'Something' received 'Nothing'' } }",
             },
           },
           stacktrace = {
             {
-              filename = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua",
+              filename = filename,
               lnum = 17,
               text = "in function <...overseer.nvim/tests/testing/lua/plenary_busted_spec.lua:5>",
             },
@@ -63,14 +68,14 @@ Errors :        0
           text = ".../overseer.nvim/tests/testing/lua/plenary_busted_spec.lua:17: Expected objects to be the same.\nPassed in:\n(table: 0x7f0d8a8f2c90) { }\nExpected:\n(table: 0x7f0d8a8f3cc8) {\n *[1] = {\n    [filename] = 'my_test.go'\n    [lnum] = 7\n    [text] = 'Expected 'Something' received 'Nothing'' } }",
         },
         {
-          id = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua:go_test parses stack traces",
-          filename = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/lua/plenary_busted_spec.lua",
+          id = string.format("%s:go_test parses stack traces", filename),
+          filename = filename,
           name = "go_test parses stack traces",
           status = TEST_STATUS.SUCCESS,
         },
         {
-          id = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/go/go_test_spec.lua:go_test parses test failures",
-          filename = "/home/stevearc/dotfiles/vimplugins/overseer.nvim/tests/testing/go/go_test_spec.lua",
+          id = string.format("%s:go_test parses test failures", filename),
+          filename = filename,
           name = "go_test parses test failures",
           status = TEST_STATUS.SUCCESS,
         },
