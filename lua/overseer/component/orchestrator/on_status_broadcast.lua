5ae185c54a20370c238afff0dafc08656a0aaccd
Steven Arcangeli <stevearc@stevearc.com>
2022-07-24 21:35:53 -0700

    fix: don't save orchestrator child tasks

--- lua/overseer/component/orchestrator/on_status_broadcast.lua
+++ lua/overseer/component/orchestrator/on_status_broadcast.lua
@@ -3,6 +3,8 @@ return {
   -- We don't allow customization of event name or params now because it's not
   -- needed. If another need arises, we can add it then.
   params = {},
+  -- Don't allow serializing/saving child tasks. The orchestrator task will recreate them if needed.
+  serialize = "fail",
   constructor = function()
     return {
       on_status = function(self, task, status)
