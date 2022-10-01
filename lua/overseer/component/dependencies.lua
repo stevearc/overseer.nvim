21db6189a06af0c9ec9f11dbe1f3eb94df454034
Steven Arcangeli <stevearc@stevearc.com>
2022-10-01 11:30:25 -0700

    fix: task dependencies with multiple of the same task name

--- lua/overseer/component/dependencies.lua
+++ lua/overseer/component/dependencies.lua
@@ -23,9 +23,9 @@ return {
       task_lookup = {},
       on_pre_start = function(self, task)
         local started_any = false
-        for _, name_or_config in ipairs(params.task_names) do
+        for i, name_or_config in ipairs(params.task_names) do
           local name, dep_params = util.split_config(name_or_config)
-          local task_id = self.task_lookup[name]
+          local task_id = self.task_lookup[i]
           local dep_task = task_id and task_list.get(task_id)
           if not dep_task then
             -- If no task ID found, start the dependency
@@ -39,7 +39,7 @@ return {
               if not new_task then
                 return
               end
-              self.task_lookup[name] = new_task.id
+              self.task_lookup[i] = new_task.id
               new_task:add_component({ "on_success_complete_dependency", task_id = task.id })
               -- Don't include child tasks when saving to bundle. We will re-create them when this
               -- task runs again
