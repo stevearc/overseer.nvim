dee3dc65f2885c55ebfcf695cb4a67d344f228d3
Steven Arcangeli <stevearc@stevearc.com>
2022-10-18 20:54:22 -0700

    feat: expand cmd in shell template (#49)

--- lua/overseer/template/shell.lua
+++ lua/overseer/template/shell.lua
@@ -7,10 +7,12 @@ local tmpl = {
     cwd = { type = "string", optional = true },
     name = { type = "string", optional = true },
     metadata = { type = "opaque", optional = true },
+    expand_cmd = { type = "boolean", default = true, optional = true },
   },
   builder = function(params)
+    local cmd = params.expand_cmd and vim.fn.expandcmd(params.cmd) or params.cmd
     return {
-      cmd = params.cmd,
+      cmd = cmd,
       env = params.env,
       cwd = params.cwd,
       name = params.name,
