b917f6a50865b592f464be0bb26add4e03b6bdaf
Steven Arcangeli <stevearc@stevearc.com>
2022-10-19 09:16:55 -0700

    feat: restart_on_save can use libuv file watchers

--- lua/overseer/component/restart_on_save.lua
+++ lua/overseer/component/restart_on_save.lua
@@ -1,4 +1,5 @@
 local files = require("overseer.files")
+local log = require("overseer.log")
 
 return {
   desc = "Restart on any buffer :write",
@@ -29,12 +30,19 @@ return {
       end,
     },
     delay = {
-      desc = "How long to wait (in ms) post-result before triggering restart",
+      desc = "How long to wait (in ms) before triggering restart",
       type = "number",
       default = 500,
       validate = function(v)
         return v > 0
       end,
+    },
+    mode = {
+      desc = "How to watch the paths",
+      type = "enum",
+      choices = { "autocmd", "uv" },
+      default = "autocmd",
+      long_desc = "'autocmd' will set autocmds on BufWritePost. 'uv' will use a libuv file watcher (recursive watching may not be supported on all platforms).",
     },
     interrupt = {
       desc = "Interrupt running tasks",
@@ -74,43 +82,89 @@ return {
       return false
     end
 
+    local restart_after_complete = false
+    local restarting = false
+    local version = 1
+    local function trigger_restart(task)
+      local trigger_version = version
+      if not restarting then
+        restarting = true
+        vim.defer_fn(function()
+          restarting = false
+          -- Only perform the restart if the version hasn't been bumped
+          if version == trigger_version then
+            if not task:restart(opts.interrupt) then
+              -- If we couldn't restart the task, it's because it is currently running and we won't
+              -- interrupt it. Flag to restart once task is complete.
+              restart_after_complete = true
+            end
+          end
+        end, opts.delay)
+      end
+    end
+
     return {
-      id = nil,
-      restart_after_complete = false,
+      autocmd_id = nil,
+      fs_events = {},
       on_init = function(self, task)
         -- This means that the task cannot be auto-disposed while this component
         -- is attached
         task:inc_reference()
-        self.id = vim.api.nvim_create_autocmd("BufWritePost", {
-          pattern = "*",
-          desc = string.format("Restart task %s on save", task.name),
-          callback = function(params)
-            -- Only care about normal files
-            if vim.api.nvim_buf_get_option(params.buf, "buftype") == "" then
-              local bufname = vim.api.nvim_buf_get_name(params.buf)
-              if is_watching_file(bufname) then
-                if not task:restart(opts.interrupt) then
-                  self.restart_after_complete = true
+        if opts.mode == "autocmd" then
+          self.autocmd_id = vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = "*",
+            desc = string.format("Restart task %s on save", task.name),
+            callback = function(params)
+              -- Only care about normal files
+              if vim.api.nvim_buf_get_option(params.buf, "buftype") == "" then
+                local bufname = vim.api.nvim_buf_get_name(params.buf)
+                if is_watching_file(bufname) then
+                  trigger_restart(task)
                 end
               end
-            end
-          end,
-        })
+            end,
+          })
+        elseif opts.mode == "uv" then
+          for _, path in ipairs(opts.paths) do
+            local fs_event = vim.loop.new_fs_event()
+            fs_event:start(
+              path,
+              { recursive = true },
+              vim.schedule_wrap(function(err, filename, events)
+                if err then
+                  log:warn("Overseer[restart_on_save] watch error: %s", err)
+                else
+                  trigger_restart(task)
+                end
+              end)
+            )
+            table.insert(self.fs_events, fs_event)
+          end
+        end
       end,
       on_reset = function(self, task)
-        self.restart_after_complete = false
+        -- Bump the version to invalidate any pending restarts
+        version = version + 1
+        restarting = false
+        restart_after_complete = false
       end,
       on_complete = function(self, task, status)
-        if self.restart_after_complete then
-          vim.schedule(function()
-            task:restart(opts.interrupt)
-          end)
+        if restart_after_complete then
+          trigger_restart(task)
         end
       end,
       on_dispose = function(self, task)
+        -- Bump the version to invalidate any pending restarts
+        version = version + 1
         task:dec_reference()
-        vim.api.nvim_del_autocmd(self.id)
-        self.id = nil
+        if self.autocmd_id then
+          vim.api.nvim_del_autocmd(self.autocmd_id)
+          self.autocmd_id = nil
+        end
+        for _, fs_event in ipairs(self.fs_events) do
+          fs_event:stop()
+        end
+        self.fs_events = {}
       end,
     }
   end,
