c426898b8d42572e8e9b4dd900160fa530f466b6
Steven Arcangeli <stevearc@stevearc.com>
2022-11-09 13:28:08 -0800

    ci: improve isolation of test running

--- .github/workflows/install_nvim.sh
+++ .github/workflows/install_nvim.sh
@@ -3,7 +3,10 @@ set -e
 PLUGINS="$HOME/.local/share/nvim/site/pack/plugins/start"
 mkdir -p "$PLUGINS"
 
-wget "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage"
+wget https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage
 chmod +x nvim.appimage
-sudo mv ./nvim.appimage /usr/bin/nvim
-git clone --depth=1 https://github.com/nvim-lua/plenary.nvim.git "$PLUGINS/plenary.nvim"
+./nvim.appimage --appimage-extract >/dev/null
+rm -f nvim.appimage
+mkdir -p ~/.local/share/nvim
+mv squashfs-root ~/.local/share/nvim/appimage
+sudo ln -s "$HOME/.local/share/nvim/appimage/AppRun" /usr/bin/nvim
