247b931a0197204952c22510f92ca2716cb276a6
Steven Arcangeli <stevearc@stevearc.com>
2022-11-12 08:50:01 -0800

    fix: parser.lib.watcher_output got stuck

--- lua/overseer/parser/lib.lua
+++ lua/overseer/parser/lib.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 ---Create a list of nodes that will parse repeating "watch" command output (e.g. tsc --watch)
----@param start_pat string Pattern that matches when we should start extracting
----@param end_pat string Pattern that matches when we should finish extracting
+---@param start_pat string|table Pattern or {opts, pattern} table that matches when we should start extracting
+---@param end_pat string Pattern or {opts, pattern} table that matches when we should finish extracting
 ---@param opts table
 ---    wrap boolean If true, wrap the resulting parser in a loop->sequence
 ---    active_on_start boolean When false, require start_pat to match before parsing errors
@@ -14,6 +14,20 @@ M.watcher_output = function(start_pat, end_pat, extraction, opts)
     active_on_start = true,
     only_clear_results_key = false,
   })
+  local end_test
+  if type(end_pat) == "table" then
+    end_test = { "test", unpack(end_pat) }
+  else
+    end_test = { "test", end_pat }
+  end
+  local skip_until_start
+  if type(start_pat) == "table" then
+    local start_opts, pat = unpack(start_pat)
+    start_opts.skip_matching_line = true
+    skip_until_start = { "skip_until", start_opts, pat }
+  else
+    skip_until_start = { "skip_until", start_pat }
+  end
   local seq = {
     {
       "always", -- When the loop exits, proceed to the next node
@@ -23,25 +37,21 @@ M.watcher_output = function(start_pat, end_pat, extraction, opts)
           "parallel",
           {
             "invert", -- Exit the loop when we detect the end of the output
-            { "test", end_pat },
+            end_test,
           },
           {
             "always", -- Don't exit the loop if extraction fails
             extraction,
           },
           -- Prevent spin-looping when extraction fails
-          { "skip_until", end_pat },
+          { "skip_lines", 1 },
         },
       },
     },
     { "dispatch", "set_results" },
   }
   local reset_seq = {
-    {
-      "skip_until", -- Ignore output until we see that the command has restarted
-      { skip_matching_line = true },
-      start_pat,
-    },
+    skip_until_start,
     { "dispatch", "clear_results", opts.only_clear_results_key },
   }
   if opts.active_on_start then
