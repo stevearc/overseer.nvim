fb71bbd1cc8e94e34f7f159fab102138cef88e1d
Laytan Laats <laytanlaats@hotmail.com>
2023-02-22 18:42:59 +0100

    style: format according to stylua

--- lua/overseer/template/builtin.lua
+++ lua/overseer/template/builtin.lua
@@ -1,1 +1,14 @@
-return { "cargo", "just", "make", "npm", "shell", "tox", "vscode", "mix", "deno", "rake", "task", "composer" }
+return {
+  "cargo",
+  "just",
+  "make",
+  "npm",
+  "shell",
+  "tox",
+  "vscode",
+  "mix",
+  "deno",
+  "rake",
+  "task",
+  "composer",
+}
