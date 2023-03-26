4812fd3ddb24dc20845321f5969fc110ab3216e5
Steven Arcangeli <stevearc@stevearc.com>
2023-03-26 13:44:55 -0700

    fix: VS Code only escape command if args are not empty (#119)

--- lua/overseer/template/vscode/provider/shell.lua
+++ lua/overseer/template/vscode/provider/shell.lua
@@ -2,9 +2,14 @@ local shell = require("overseer.shell")
 local M = {}
 
 M.get_task_opts = function(defn)
-  local command = vim.list_extend({ defn.command }, defn.args or {})
+  local command = defn.command
+  -- Only perform escaping if args is not empty
+  if defn.args and not vim.tbl_isempty(defn.args) then
+    local cmd_list = vim.list_extend({ defn.command }, defn.args)
+    command = shell.escape_cmd(cmd_list)
+  end
   return {
-    cmd = shell.escape_cmd(command),
+    cmd = command,
   }
 end
 
