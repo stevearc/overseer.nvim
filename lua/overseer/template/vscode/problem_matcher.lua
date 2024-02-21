4855aefcf335bbac71eea9c6a888958fb1ed1e1a
Steven Arcangeli <stevearc@stevearc.com>
2024-02-20 18:04:10 -0800

    fix: eslint and jshint problem matcher patterns (#260)
    This fixes the resolution order of named patterns (e.g.
    `$eslint-stylish`) so that we look it up and resolve it into a table
    _before_ we check to see if the pattern is list-like or map-like.
    
    This also fixes some of the vim regular expressions to use the correct
    syntax for non-greedy matching

--- lua/overseer/template/vscode/problem_matcher.lua
+++ lua/overseer/template/vscode/problem_matcher.lua
@@ -60,7 +60,7 @@ local default_patterns = {
   },
   ["$jshint"] = {
     regexp = "^(.*):\\s+line\\s+(\\d+),\\s+col\\s+(\\d+),\\s(.+?)(?:\\s+\\((\\w)(\\d+)\\))?$",
-    vim_regexp = "\\v^(.*):\\s+line\\s+(\\d+),\\s+col\\s+(\\d+),\\s(.+?)%(\\s+\\((\\w)(\\d+)\\))?$",
+    vim_regexp = "\\v^(.*):\\s+line\\s+(\\d+),\\s+col\\s+(\\d+),\\s(.{-1,})%(\\s+\\((\\w)(\\d+)\\))?$",
     kind = "location",
     file = 1,
     line = 2,
@@ -77,7 +77,7 @@ local default_patterns = {
     },
     {
       regexp = "^\\s+line\\s+(\\d+)\\s+col\\s+(\\d+)\\s+(.+?)(?:\\s+\\((\\w)(\\d+)\\))?$",
-      vim_regexp = "\\v^\\s+line\\s+(\\d+)\\s+col\\s+(\\d+)\\s+(.+?)%(\\s+\\((\\w)(\\d+)\\))?$",
+      vim_regexp = "\\v^\\s+line\\s+(\\d+)\\s+col\\s+(\\d+)\\s+(.{-1,})%(\\s+\\((\\w)(\\d+)\\))?$",
       line = 1,
       character = 2,
       message = 3,
@@ -99,13 +99,13 @@ local default_patterns = {
   ["$eslint-stylish"] = {
     {
       regexp = "^((?:[a-zA-Z]:)*[./\\\\]+.*?)$",
-      vim_regexp = "\\v^(%([a-zA-Z]:)*[./\\\\]+.*?)$",
+      vim_regexp = "\\v^(%([a-zA-Z]:)*[./\\\\]+.{-})$",
       kind = "location",
       file = 1,
     },
     {
       regexp = "^\\s+(\\d+):(\\d+)\\s+(error|warning|info)\\s+(.+?)(?:\\s\\s+(.*))?$",
-      vim_regexp = "\\v^\\s+(\\d+):(\\d+)\\s+(error|warning|info)\\s+(.+?)%(\\s\\s+(.*))?$",
+      vim_regexp = "\\v^\\s+(\\d+):(\\d+)\\s+(error|warning|info)\\s+(.{-1,})%(\\s\\s+(.*))?$",
       line = 1,
       character = 2,
       severity = 3,
@@ -358,14 +358,6 @@ end
 
 local function convert_pattern(pattern, opts)
   opts = opts or {}
-  if type(pattern) == "string" then
-    if default_patterns[pattern] then
-      pattern = vim.deepcopy(default_patterns[pattern])
-    else
-      log:error("Could not find problem matcher pattern '%s'", pattern)
-      return nil
-    end
-  end
   local args = {}
   local full_line_key
   local max_arg = 0
@@ -550,6 +542,15 @@ M.get_parser_from_problem_matcher = function(problem_matcher, precalculated_vars
   local convert = problem_matcher.fileLocation
     and file_converter(problem_matcher.fileLocation, precalculated_vars)
   local ret
+  if type(pattern) == "string" then
+    if default_patterns[pattern] then
+      pattern = vim.deepcopy(default_patterns[pattern])
+    else
+      log:error("Could not find problem matcher pattern '%s'", pattern)
+      return nil
+    end
+  end
+
   if vim.tbl_islist(pattern) then
     ret = { "sequence" }
     for i, v in ipairs(pattern) do
