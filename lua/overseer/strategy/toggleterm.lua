1f5f271e00b82ced6a30ae5ad6dbe7d1104e5980
Steven Arcangeli <stevearc@stevearc.com>
2024-06-04 07:56:50 -0700

    fix(toggleterm): various issues with toggleterm strategy

--- lua/overseer/strategy/toggleterm.lua
+++ lua/overseer/strategy/toggleterm.lua
@@ -4,24 +4,28 @@ local util = require("overseer.util")
 
 local terminal = require("toggleterm.terminal")
 
+---@class overseer.ToggleTermStrategy : overseer.Strategy
+---@field private opts overseeer.ToggleTermStrategyOpts
 local ToggleTermStrategy = {}
 
+---@class overseeer.ToggleTermStrategyOpts
+---@field use_shell? boolean load user shell before running task
+---@field direction? "vertical"|"horizontal"|"tab"|"float"
+---@field highlights? table map to a highlight group name and a table of it's values
+---@field auto_scroll? boolean automatically scroll to the bottom on task output
+---@field close_on_exit? boolean close the terminal and delete terminal buffer (if open) after task exits
+---@field quit_on_exit? "never"|"always"|"success" close the terminal window (if open) after task exits
+---@field open_on_start? boolean toggle open the terminal automatically when task starts
+---@field hidden? boolean cannot be toggled with normal ToggleTerm commands
+---@field on_create? fun(term: table) function to execute on terminal creation
+
 ---Run tasks using the toggleterm plugin
----@param opts nil|table
----    use_shell nil|boolean load user shell before running task
----    direction nil|"vertical"|"horizontal"|"tab"|"float"
----    highlights nil|table map to a highlight group name and a table of it's values
----    auto_scroll nil|boolean automatically scroll to the bottom on task output
----    close_on_exit nil|boolean close the terminal and delete terminal buffer (if open) after task exits
----    quit_on_exit "never"|"always"|"success" close the terminal window (if open) after task exits
----    open_on_start nil|boolean toggle open the terminal automatically when task starts
----    hidden nil|boolean cannot be toggled with normal ToggleTerm commands
----    on_create nil|fun(term: table) function to execute on terminal creation
+---@param opts? overseeer.ToggleTermStrategyOpts
 ---@return overseer.Strategy
 function ToggleTermStrategy.new(opts)
   opts = vim.tbl_extend("keep", opts or {}, {
     use_shell = false,
-    direction = nil,
+    direction = "float",
     highlights = nil,
     auto_scroll = nil,
     close_on_exit = false,
@@ -31,32 +35,24 @@ function ToggleTermStrategy.new(opts)
     on_create = nil,
   })
   return setmetatable({
-    bufnr = nil,
-    chan_id = nil,
     opts = opts,
     term = nil,
   }, { __index = ToggleTermStrategy })
 end
 
 function ToggleTermStrategy:reset()
-  util.soft_delete_buf(self.bufnr)
-  self.bufnr = nil
-  if self.chan_id then
-    vim.fn.jobstop(self.chan_id)
-    self.chan_id = nil
-  end
   if self.term then
-    self.term:close()
+    self.term:shutdown()
+    self.term = nil
   end
 end
 
 function ToggleTermStrategy:get_bufnr()
-  return self.bufnr
+  return self.term and self.term.bufnr
 end
 
 ---@param task overseer.Task
 function ToggleTermStrategy:start(task)
-  local chan_id
   local mode = vim.api.nvim_get_mode().mode
   local stdout_iter = util.get_stdout_line_iter()
 
@@ -88,6 +84,9 @@ function ToggleTermStrategy:start(task)
     close_on_exit = self.opts.close_on_exit,
     hidden = self.opts.hidden,
     on_create = function(t)
+      local job_id = t.job_id
+      jobs.register(job_id)
+
       if self.opts.on_create then
         self.opts.on_create(t)
       end
@@ -97,26 +96,25 @@ function ToggleTermStrategy:start(task)
         t:send("exit $?")
       end
     end,
-    on_stdout = function(_job, job_id, d)
-      if self.chan_id ~= job_id then
+    on_stdout = function(t, job_id, d)
+      if t ~= self.term then
         return
       end
       on_stdout(d)
     end,
     on_exit = function(t, j, c)
       jobs.unregister(j)
-      if self.chan_id ~= j then
+      if t ~= self.term then
         return
       end
       -- Feed one last line end to flush the output
       on_stdout({ "" })
-      self.chan_id = nil
       if vim.v.exiting == vim.NIL then
         task:on_exit(c)
       end
 
       local close = self.opts.quit_on_exit == "always"
-      close = close or (self.opts.quit_on_exit == "success" and c == 0)
+        or (self.opts.quit_on_exit == "success" and c == 0)
       if close then
         t:close()
       end
@@ -129,32 +127,20 @@ function ToggleTermStrategy:start(task)
     self.term:spawn()
   end
 
-  chan_id = self.term.job_id
-  self.bufnr = self.term.bufnr
-  vim.b[self.bufnr].overseer_task = task.id
-
   util.hack_around_termopen_autocmd(mode)
-
-  if chan_id == 0 then
-    error(string.format("Invalid arguments for task '%s'", task.name))
-  elseif chan_id == -1 then
-    error(string.format("Command '%s' not executable", vim.inspect(task.cmd)))
-  else
-    jobs.register(chan_id)
-    self.chan_id = chan_id
-  end
 end
 
 function ToggleTermStrategy:stop()
-  if self.chan_id then
-    vim.fn.jobstop(self.chan_id)
-    self.chan_id = nil
+  if self.term and self.term.job_id then
+    vim.fn.jobstop(self.term.job_id)
   end
 end
 
 function ToggleTermStrategy:dispose()
-  self:stop()
-  util.soft_delete_buf(self.bufnr)
+  if self.term then
+    self.term:shutdown()
+    self.term = nil
+  end
 end
 
 return ToggleTermStrategy
