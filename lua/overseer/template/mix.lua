stevearc/overseer.nvim

commit 2fdd59fb8cbde30e65bc72208b1f133d9e5490d4

refactor: allow error reporting in template generator

lua/overseer/template/mix.lua
@@ -1,77 +1,44 @@
-local log = require("overseer.log")
-local overseer = require("overseer")
-
----@type overseer.TemplateFileDefinition
-local tmpl = {
-  params = {
-    subcmd = { optional = true },
-    args = { type = "list", delimiter = " ", default = {} },
-  },
-  builder = function(params)
-    local cmd = { "mix" }
-    local args = params.args or {}
-    if params.subcmd then
-      table.insert(args, 1, params.subcmd)
-    end
-    return {
-      cmd = cmd,
-      args = args,
-    }
-  end,
-}
-
 ---@param opts overseer.SearchParams
 ---@return nil|string
 local function get_mix_file(opts)
   return vim.fs.find("mix.exs", { upward = true, type = "file", path = opts.dir })[1]
 end
 
+---@type overseer.TemplateFileProvider
 return {
   cache_key = function(opts)
     return get_mix_file(opts)
   end,
-  condition = {
-    callback = function(opts)
-      if not get_mix_file(opts) then
-        return false, "No mix.exs file found"
-      end
-      return true
-    end,
-  },
   generator = function(opts, cb)
     -- mix will not return all the tasks unless you invoke it in the mix.exs folder
-    local mix_folder = vim.fs.dirname(assert(get_mix_file(opts)))
-    local ret = {}
-    local jid = vim.fn.jobstart({
-      "mix",
-      "help",
-    }, {
-      cwd = mix_folder,
-      stdout_buffered = true,
-      on_stdout = vim.schedule_wrap(function(j, output)
-        for _, line in ipairs(output) do
-          local task_name = line:match("mix (%S+)%s")
-          table.insert(
-            ret,
-            overseer.wrap_template(
-              tmpl,
-              { name = string.format("mix %s", task_name) },
-              { subcmd = task_name }
-            )
-          )
-        end
-        table.insert(ret, overseer.wrap_template(tmpl, { name = "mix" }))
-      end),
-      on_exit = vim.schedule_wrap(function(j, output)
-        cb(ret)
-      end),
-    })
-    if jid == 0 then
-      log.error("Passed invalid arguments to 'mix'")
-      cb(ret)
-    elseif jid == -1 then
-      log.error("'mix' is not executable")
-      cb(ret)
+    local mix_file = get_mix_file(opts)
+    if not mix_file then
+      return "No mix.exs file found"
     end
+    local mix_folder = vim.fs.dirname(mix_file)
+    local ret = {}
+    vim.system(
+      { "mix", "help" },
+      {
+        cwd = mix_folder,
+        text = true,
+      },
+      vim.schedule_wrap(function(out)
+        if out.code ~= 0 then
+          return cb(out.stderr or out.stdout or "Error running 'mix help'")
+        end
+        for line in vim.gsplit(out.stdout, "\n") do
+          local task_name = line:match("mix (%S+)%s")
+          table.insert(ret, {
+            name = string.format("mix %s", task_name),
+            builder = function()
+              return {
+                cmd = { "mix", task_name },
+              }
+            end,
+          })
+        end
+      end)
+    )
   end,
 }

lua/overseer/commands.lua
@@ -289,38 +289,11 @@ M.task_action = function()
   end)
 end
 
+---@param callback fun(info: overseer.Report)
 M.info = function(callback)
   local search_opts = get_search_params()
-  local info = {
-    log = {
-      file = nil,
-      level = nil,
-    },
-    templates = {
-      templates = {},
-      providers = {},
-    },
-  }
-  local levels_reverse = {}
-  for k, v in pairs(vim.log.levels) do
-    levels_reverse[v] = k
-  end
-  for _, log_conf in ipairs(config.log) do
-    if log_conf.type == "file" then
-      local ok, stdpath = pcall(vim.fn.stdpath, "log")
-      if not ok then
-        stdpath = vim.fn.stdpath("cache")
-      end
-      info.log = {
-        file = files.join(stdpath, log_conf.filename),
-        level = levels_reverse[log_conf.level],
-      }
-      break
-    end
-  end
   template.list(search_opts, function(_, report)
-    info.templates = report
-    callback(info)
+    callback(report)
   end)
 end
 

lua/overseer/health.lua
@@ -22,6 +22,7 @@ M.check = function()
   vim.health.info(string.format("Log file: %s", log.get_logfile()))
   vim.health.info(string.format("Log level: %s", level_map[config.log_level]))
 
+  ---@type overseer.Report
   local info
   commands.info(function(info_cb)
     info = info_cb
@@ -36,28 +37,43 @@ M.check = function()
     return
   end
 
-  for name, tmpl_report in pairs(info.templates.templates) do
-    if tmpl_report.is_present then
-      vim.health.ok(string.format("%s: available", name))
-    else
+  for name, tmpl_report in pairs(info.templates) do
+    if tmpl_report.message then
       vim.health.warn(string.format("%s: %s", name, tmpl_report.message))
+    else
+      vim.health.ok(string.format("%s: available", name))
     end
   end
-  for name, provider_report in pairs(info.templates.providers) do
-    if provider_report.is_present then
+  local provider_names = vim.tbl_keys(info.providers)
+  table.sort(provider_names, function(a_name, b_name)
+    local a, b = info.providers[a_name], info.providers[b_name]
+    local a_err, b_err = a.message ~= nil, b.message ~= nil
+    if a_err ~= b_err then
+      return not a_err
+    end
+    return a_name < b_name
+  end)
+  for _, name in ipairs(provider_names) do
+    local provider_report = info.providers[name]
+    if name:match("^[%w_%.%-]+$") then
+      name = "{" .. name .. "}"
+    end
+    if provider_report.message then
+      vim.health.warn(string.format("%s: %s", name, provider_report.message))
+    else
       if provider_report.from_cache then
         name = name .. " (cached)"
+      elseif provider_report.elapsed_ms > 0 then
+        name = string.format("%s (%sms)", name, provider_report.elapsed_ms)
       end
       vim.health.ok(
         string.format(
-          "%s: %d/%d tasks available",
+          "%s: `%d/%d tasks available`",
           name,
           provider_report.available_tasks,
           provider_report.total_tasks
         )
       )
-    else
-      vim.health.warn(string.format("%s: %s", name, provider_report.message))
     end
   end
 end

lua/overseer/template/init.lua
@@ -9,16 +9,16 @@ local util = require("overseer.util")
 local islist = vim.islist or vim.tbl_islist
 local M = {}
 
----@class overseer.TemplateFileProvider
+---@class (exact) overseer.TemplateFileProvider
 ---@field module? string The name of the module this was loaded from
 ---@field condition? overseer.SearchCondition
 ---@field cache_key? fun(opts: overseer.SearchParams): nil|string
----@field generator fun(opts: overseer.SearchParams, cb: fun(tmpls: overseer.TemplateDefinition[]))
+---@field generator fun(opts: overseer.SearchParams, cb: fun(tmpls_or_err: string|overseer.TemplateDefinition[])) : nil|string|overseer.TemplateDefinition[]
 
----@class overseer.TemplateProvider : overseer.TemplateFileProvider
+---@class (exact) overseer.TemplateProvider : overseer.TemplateFileProvider
 ---@field name string
 
----@class overseer.TemplateFileDefinition
+---@class (exact) overseer.TemplateFileDefinition
 ---@field module? string The name of the module this was loaded from
 ---@field aliases? string[]
 ---@field desc? string
@@ -28,10 +28,10 @@ local M = {}
 ---@field builder fun(params: table): overseer.TaskDefinition
 ---@field hide? boolean Hide from the template list
 
----@class overseer.TemplateDefinition : overseer.TemplateFileDefinition
+---@class (exact) overseer.TemplateDefinition : overseer.TemplateFileDefinition
 ---@field name string
 
----@class overseer.SearchCondition
+---@class (exact) overseer.SearchCondition
 ---@field filetype? string|string[]
 ---@field dir? string|string[]
 ---@field callback? fun(search: overseer.SearchParams): boolean, nil|string
@@ -390,8 +390,19 @@ M.clear_cache = function(opts)
   end
 end
 
+---@class (exact) overseer.Report
+---@field providers table<string, overseer.ProviderReport>
+---@field templates table<string, {message: nil|string}>
+
+---@class (exact) overseer.ProviderReport
+---@field message? string
+---@field from_cache? boolean
+---@field total_tasks integer
+---@field available_tasks integer
+---@field elapsed_ms integer
+
 ---@param opts overseer.SearchParams
----@param cb fun(templates: overseer.TemplateDefinition[], report: table)
+---@param cb fun(templates: overseer.TemplateDefinition[], report: overseer.Report)
 M.list = function(opts, cb)
   initialize()
   vim.validate({
@@ -417,6 +428,7 @@ M.list = function(opts, cb)
   end
 
   local ret = {}
+  ---@type overseer.Report
   local report = {
     templates = {},
     providers = {},
@@ -428,7 +440,6 @@ M.list = function(opts, cb)
       table.insert(ret, tmpl)
     end
     report.templates[tmpl.name] = {
-      is_present = is_match,
       message = message,
     }
   end
@@ -447,20 +458,21 @@ M.list = function(opts, cb)
   local start_times = {}
   local timed_out = false
   ---This is the async callback that is passed to generators
-  ---@param tmpls overseer.TemplateDefinition[]
+  ---@param tmpls_or_err string|overseer.TemplateDefinition[]
   ---@param provider_name string
   ---@param module nil|string
   ---@param cache_key nil|string
   ---@param from_cache nil|boolean
-  local function handle_tmpls(tmpls, provider_name, module, cache_key, from_cache)
-    local elapsed_ms = (vim.uv.hrtime() - start_times[provider_name]) / 1e6
+  local function handle_tmpls(tmpls_or_err, provider_name, module, cache_key, from_cache)
+    local elapsed_ms = (vim.uv.now() - start_times[provider_name])
     if
       cache_key
       and config.template_cache_threshold > 0
       and elapsed_ms >= config.template_cache_threshold
+      and type(tmpls_or_err) == "table"
     then
-      log.debug("Caching %s: [%s] = %d", provider_name, cache_key, #tmpls)
-      cached_provider_results[cache_key] = tmpls
+      log.debug("Caching %s: [%s] = %d", provider_name, cache_key, #tmpls_or_err)
+      cached_provider_results[cache_key] = tmpls_or_err
     end
     if not pending[provider_name] then
       if not timed_out then
@@ -470,26 +482,37 @@ M.list = function(opts, cb)
     end
     pending[provider_name] = nil
     local num_available = 0
-    for _, tmpl in ipairs(tmpls) do
-      -- Set the module on the template so it can be used to match hooks
-      tmpl.module = module
-      local ok, err = pcall(validate_template_definition, tmpl)
-      if ok then
-        if condition_matches(tmpl.condition, tmpl.tags, opts, true) then
-          num_available = num_available + 1
-          table.insert(ret, tmpl)
+
+    if type(tmpls_or_err) == "string" then
+      report.providers[provider_name] = {
+        message = tmpls_or_err,
+        from_cache = false,
+        total_tasks = 0,
+        available_tasks = 0,
+        elapsed_ms = elapsed_ms,
+      }
+    else
+      for _, tmpl in ipairs(tmpls_or_err) do
+        -- Set the module on the template so it can be used to match hooks
+        tmpl.module = module
+        local ok, err = pcall(validate_template_definition, tmpl)
+        if ok then
+          if condition_matches(tmpl.condition, tmpl.tags, opts, true) then
+            num_available = num_available + 1
+            table.insert(ret, tmpl)
+          end
+        else
+          log.error("Template %s from %s: %s", tmpl.name, provider_name, err)
         end
-      else
-        log.error("Template %s from %s: %s", tmpl.name, provider_name, err)
       end
+      report.providers[provider_name] = {
+        message = nil,
+        from_cache = from_cache,
+        total_tasks = #tmpls_or_err,
+        available_tasks = num_available,
+        elapsed_ms = elapsed_ms,
+      }
     end
-    report.providers[provider_name] = {
-      is_present = true,
-      message = nil,
-      from_cache = from_cache,
-      total_tasks = #tmpls,
-      available_tasks = num_available,
-    }
 
     final_callback()
   end
@@ -513,10 +536,20 @@ M.list = function(opts, cb)
     local is_match, message = condition_matches(provider.condition, nil, opts, false)
     if is_match then
       local cache_key = provider.cache_key(opts)
-      local provider_cb = function(tmpls)
-        handle_tmpls(tmpls, provider_name, provider.module, cache_key)
+      local provider_done = false
+      ---@param tmpls_or_err string|overseer.TemplateDefinition[]
+      local provider_cb = function(tmpls_or_err)
+        if provider_done then
+          log.error(
+            "Template provider %s: generator callback called twice. This can also happen if you return results from the function and call the callback.",
+            provider.name
+          )
+        else
+          provider_done = true
+          handle_tmpls(tmpls_or_err, provider_name, provider.module, cache_key)
+        end
       end
-      start_times[provider.name] = vim.uv.hrtime()
+      start_times[provider.name] = vim.uv.now()
       pending[provider.name] = true
       if cache_key and cached_provider_results[cache_key] then
         handle_tmpls(
@@ -531,19 +564,21 @@ M.list = function(opts, cb)
         if ok then
           if tmpls then
             -- if there was a return value, the generator completed synchronously
-            -- TODO deprecate this flow
             provider_cb(tmpls)
           end
         else
+          assert(type(tmpls) == "string")
           log.error("Template provider %s: %s", provider.name, tmpls)
+          local errmsg = vim.split(tmpls, "\n", { plain = true })[1]
+          provider_cb(errmsg)
         end
       end
     else
       report.providers[provider_name] = {
-        is_present = is_match,
         message = message,
         total_tasks = 0,
         available_tasks = 0,
+        elapsed_ms = 0,
       }
     end
   end

lua/overseer/template/make.lua
@@ -9,19 +9,14 @@ local provider = {
   cache_key = function(opts)
     return get_makefile(opts)
   end,
-  condition = {
-    callback = function(opts)
-      if vim.fn.executable("make") == 0 then
-        return false, 'Command "make" not found'
-      end
-      if not get_makefile(opts) then
-        return false, "No Makefile found"
-      end
-      return true
-    end,
-  },
   generator = function(opts, cb)
-    local makefile = assert(get_makefile(opts))
+    if vim.fn.executable("make") == 0 then
+      return 'Command "make" not found'
+    end
+    local makefile = get_makefile(opts)
+    if not makefile then
+      return "No Makefile found"
+    end
     local cwd = vim.fs.dirname(makefile)
 
     local ret = {}
@@ -36,7 +31,7 @@ local provider = {
       },
       vim.schedule_wrap(function(out)
         if out.code ~= 0 and out.code ~= 1 then
-          return cb({})
+          return cb(out.stderr or out.stdout or "Error running 'make'")
         end
 
         local parsing = false

commit 1936ae5abd16d9015f86201558bc6b98352ffa10

fix(mix): call callback after successfully parsing tasks
The mix template provider was missing the cb(ret) call after the for
loop that parses mix help output. This caused the provider to never
complete on success, resulting in a 3-second timeout delay.

Looks like 2fdd59fb8cbde30e65bc72208b1f133d9e5490d4 caused it.

lua/overseer/template/mix.lua
@@ -39,6 +39,7 @@ return {
             end,
           })
         end
+        cb(ret)
       end)
     )
   end,